3640bca687bba25efd465867da3b5b4cc9af4695
Pedro Henrique de Oliveira <pedro.pho792@gmail.com>
2025-11-06 16:44:58 -0300

    feat: add compare hash tests

--- src/infra/gatways/bcrypti-handler.ts
+++ src/infra/gatways/bcrypti-handler.ts
@@ -1,8 +1,12 @@
-import { hash } from "bcryptjs";
-import { HashPassword } from "@/domain/contracts/gateways";
+import { compare, hash } from "bcryptjs";
+import { ComparePassword, HashPassword } from "@/domain/contracts/gateways";
 
-export class BcryptHandler implements HashPassword {
+export class BcryptHandler implements HashPassword, ComparePassword {
   async hash(input: HashPassword.Input): Promise<string> {
     return hash(input.password, 6);
+  }
+
+  async compare(input: ComparePassword.Input): Promise<boolean> {
+    return compare(input.password, input.passwordHashed);
   }
 }
--- tests/infra/gateways/bcrypti-handler.test.ts
+++ tests/infra/gateways/bcrypti-handler.test.ts
@@ -13,17 +13,42 @@ describe("BCRYPT HANDLER", () => {
 
   beforeEach(async () => {
     fakeBcrypt.hash.mockImplementation(() => "any_password_hash");
+    fakeBcrypt.compare.mockImplementation(() => true);
     sut = new BcryptHandler();
   });
 
-  it("Should call hash with correct input", async () => {
-    await sut.hash({ password: "any_password" });
-    expect(fakeBcrypt.hash).toHaveBeenCalledWith("any_password", 6);
-    expect(fakeBcrypt.hash).toHaveBeenCalledTimes(1);
+  describe("HASH", () => {
+    it("Should call hash with correct input", async () => {
+      await sut.hash({ password: "any_password" });
+      expect(fakeBcrypt.hash).toHaveBeenCalledWith("any_password", 6);
+      expect(fakeBcrypt.hash).toHaveBeenCalledTimes(1);
+    });
+
+    it("Should return a password hash", async () => {
+      const passwordHash = await sut.hash({ password: "any_password" });
+      expect(passwordHash).toBe("any_password_hash");
+    });
   });
 
-  it("Should return a password hash", async () => {
-    const passwordHash = await sut.hash({ password: "any_password" });
-    expect(passwordHash).toBe("any_password_hash");
+  describe("COMPARE", () => {
+    it("Should call compare with correct input", async () => {
+      await sut.compare({
+        password: "any_password",
+        passwordHashed: "any_password_hash",
+      });
+      expect(fakeBcrypt.compare).toHaveBeenCalledWith(
+        "any_password",
+        "any_password_hash",
+      );
+      expect(fakeBcrypt.compare).toHaveBeenCalledTimes(1);
+    });
+
+    it("Should return a boolean", async () => {
+      const result = await sut.compare({
+        password: "any_password",
+        passwordHashed: "any_password_hash",
+      });
+      expect(result).toBe(true);
+    });
   });
 });
